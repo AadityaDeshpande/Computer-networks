7edd3ba673c57b65bf197d97b84b14566dfdfdbd
Aaditya Deshpande <41305804+AadityaDeshpande@users.noreply.github.com>
2018-09-02 17:31:02 +0530

    Add files via upload
    udp protocol 
    1)peer to peer chat 
    2)multi user chat

---
+++ UDPServerP.java
@@ -0,0 +1,49 @@
+//udp server java
+import java.io.*; 
+import java.net.*; 
+  
+class UDPServerP { 
+  public static void main(String args[]) throws Exception 
+    { 
+  
+      DatagramSocket serverSocket = new DatagramSocket(9876); 
+       BufferedReader outFromser = 
+        new BufferedReader(new InputStreamReader(System.in)); 
+  
+      byte[] receiveData = new byte[1024]; 
+      byte[] sendData  = new byte[1024]; 
+  
+      while(true) 
+        { 
+  
+          DatagramPacket receivePacket = 
+             new DatagramPacket(receiveData, receiveData.length); 
+           serverSocket.receive(receivePacket); 
+           
+          String sentence = new String(receivePacket.getData()); 
+  
+          //System.out.print(sentence);
+
+          InetAddress IPAddress = receivePacket.getAddress(); 
+ 
+          System.out.print("msg from client is = " +sentence +"\n" );
+  
+          int port = receivePacket.getPort(); 
+        
+          String word = outFromser.readLine();
+  
+         // String capitalizedSentence = sentence.toUpperCase(); 
+
+          sendData = word.getBytes(); 
+        
+         
+  
+    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress,port); 
+        
+
+          serverSocket.send(sendPacket);
+  //   DatagramPacket sendPacket2 = new DatagramPacket(sendData, sendData.length, IPAddress,port); 
+ 
+        } 
+    } 
+}  
